92401afa41d40f96494b4c1cf85b5f6023296be2
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-09-20 22:02:25 +0530

    Modify Model Names Case type

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,27 +42,27 @@ class ModelTrainer:
             )
 
             models = {
-                "Random Forest" : RandomForestRegressor(),
-                "Decision Tree": DecisionTreeRegressor(),
-                "Gradient Boosting": GradientBoostingRegressor(),
-                "Linear Regression": LinearRegression(),
-                "XGB Regressor": XGBRegressor(),
-                "CatBoost Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor()
+                "RandomForest" : RandomForestRegressor(),
+                "DecisionTree": DecisionTreeRegressor(),
+                "GradientBoosting": GradientBoostingRegressor(),
+                "LinearRegression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoostRegressor": CatBoostRegressor(verbose=False),
+                "AdaBoostRegressor": AdaBoostRegressor()
             }
             params = {
-                "Decision Tree": {
+                "DecisionTree": {
                     'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                     # 'splitter':['best','random'],
                     # 'max_features':['sqrt','log2'],
                 },
-                "Random Forest": {
+                "RandomForest": {
                     # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
 
                     # 'max_features':['sqrt','log2',None],
                     'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "Gradient Boosting": {
+                "GradientBoosting": {
                     # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
                     'learning_rate': [.1, .01, .05, .001],
                     'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
@@ -70,17 +70,17 @@ class ModelTrainer:
                     # 'max_features':['auto','sqrt','log2'],
                     'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "Linear Regression": {},
-                "XGB Regressor": {
+                "LinearRegression": {},
+                "XGBRegressor": {
                     'learning_rate': [.1, .01, .05, .001],
                     'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "CatBoost Regressor": {
+                "CatBoostRegressor": {
                     'depth': [6, 8, 10],
                     'learning_rate': [0.01, 0.05, 0.1],
                     'iterations': [30, 50, 100]
                 },
-                "AdaBoost Regressor": {
+                "AdaBoostRegressor": {
                     'learning_rate': [.1, .01, 0.5, .001],
                     # 'loss':['linear','square','exponential'],
                     'n_estimators': [8, 16, 32, 64, 128, 256]
